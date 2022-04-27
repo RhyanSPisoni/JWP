e4e996cb7d5957f73afe28e36aa3ef9ce813d92d
RhyanSPisoni <rhyanoficial14@gmail.com>
2022-04-27 19:55:52 -0300

    Att.

--- TrabalhoJPW/app.js
+++ TrabalhoJPW/app.js
@@ -1,8 +1,11 @@
+const api = require('./gameapi')
+
 const express = require('express');
 const app = express();
 const path = require("path")
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { randomInt } = require('crypto');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,6 +16,26 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', async (req, res, next) => {
 
+})
+
+app.get('/game', async (req, res, next) => {
+    try {
+        const { data } = await api.get('all.json')
+
+        var listaid = []
+
+        data.forEach(e => {
+            listaid.push(e.id)
+        });
+        const numeroplayer1 = Math.floor(Math.random() * listaid.length);
+        const numeroplayer2 = Math.floor(Math.random() * listaid.length);
+
+        var player1 = data.indexOf(numeroplayer1)
+        console.log(player1)
+
+    } catch (error) {
+        res.send({ error: error.message })
+    }
 })
 
 app.post('/login', async (req, res, next) => {
---
+++ TrabalhoJPW/gameapi.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+const api = axios.create({
+    baseURL: "https://akabab.github.io/superhero-api/api/",
+});
+
+module.exports = api;
