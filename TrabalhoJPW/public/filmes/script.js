69e542727ec366af2e8282fc14d77665e0364042
RhyanSPisoni <rhyanoficial14@gmail.com>
2022-04-28 17:33:35 -0300

    Att

--- TrabalhoJPW/public/filmes/script.js
+++ TrabalhoJPW/public/filmes/script.js
@@ -1,43 +1,58 @@
-//Filmes
-var obj = [
+var filmes = [
     {
-        nome: "Vingadores Ultimato",
-        sinopse: "Um filme da Marvel",
-        personsagens: [
-            {
-                nome: "Thanos",
-                img: ""
-            },
-            {
-                nome: "Capitão America",
-                img: ""
-            }
-        ]
+        NomeFilme: "Eternos",
+        Sinopse: "Os Eternos são uma raça de seres imortais que viveram durante a antiguidade da Terra, moldando sua história e suas civilizações enquanto batalhavam os malignos Deviantes.",
+        Imagens: "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/11/eternos-capa-2.jpg",
+        Personagens: [
+            Nome = "Ikaris",
+            Descricao = "Ikaris é um personagem das histórias em quadrinhos do Universo Marvel, publicado pela Marvel Comics. Ele apareceu pela primeira vez em The Eternals # 1 e foi criado por Jack Kirby. Ele é um membro dos Eternos, uma raça de super-humanos do Universo",
+            LinkImagem = "https://rollingstone.uol.com.br/media/_versions/richard_madden_como_ikaris_em_eternos_foto_reproducao__twitter_widelg.jpg"
+        ],
+    },
+    {
+        NomeFilme: "Vingadores: Ultimato",
+        Sinopse: "Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.",
+        Imagens: "https://www.blogsoestado.com/marciohenrique/files/2019/09/VINGADORES-ULTIMATO-2019.jpg",
+        Personagens: [
+            Nome = "Thanos",
+            Descricao = "Thanos é um personagem fictício, um supervilão das histórias em quadrinhos publicadas pela Marvel Comics, inspirado em Thanatos",
+            LinkImagem = "https://uploads.jovemnerd.com.br/wp-content/uploads/2018/04/thanos-hot-toy-1210x540.jpg"
+        ],
     }
 ]
 
+const obj = filmes;
+
+console.log(obj)
+
+let ul = document.createElement('ul');
+document.getElementById('listaFilmes').appendChild(ul);
 obj.forEach(e => {
-    let nomeFilme = document.createElement('h2');
-    document.getElementById('filmes').appendChild(nomeFilme);
-    nomeFilme.innerHTML += e.nome;
+    let li = document.createElement('li');
+    ul.appendChild(li);
 
-    e.personsagens.forEach(e => {
-
-        let nome = document.createElement('h5');
-        document.getElementById('filmes').appendChild(nome);
-        nome.innerHTML += e.nome;
-
-        // fetch(e.img)
-        //     .then(data => {
-        //         return data.blob();
-        //     }).then(res => {
-        //         const imgURL = URL.createObjectURL(res)
-        //         const img = `<img src="${imgURL}"/>`;
-        //         document.querySelector('#listapersonagensimgfilmes').innerHTML = img;
-        //     })
-    })
+    li.innerHTML += e.NomeFilme;
 });
 
+
+obj.forEach(e => {
+    let nomeFilme = document.createElement('h1');
+    document.getElementById('filme').appendChild(nomeFilme);
+    nomeFilme.innerHTML += '|' + e.NomeFilme + '|';
+
+    let sinopse = document.createElement('h3');
+    document.getElementById('filme').appendChild(sinopse);
+    sinopse.innerHTML += '- ' + e.Sinopse
+
+    fetch(e.Imagens)
+        .then(data => {
+            return data.blob();
+        }).then(res => {
+            const imgURL = URL.createObjectURL(res)
+            const img = `<img src="${imgURL}"/>`;
+            document.querySelector('#filme').innerHTML = img;
+        })
+});
 
 fetch("https://akabab.github.io/superhero-api/api/all.json")
     .then(data => {
@@ -66,3 +81,7 @@ fetch("https://akabab.github.io/superhero-api/api/all.json")
                 })
             })
     })
+
+
+
+
--- WikiHeroes/script.js
+++ WikiHeroes/script.js
@@ -1,38 +1,25 @@
-var filmes =
-    '[{' +
-    '"NomeFilme": "Eternos",' +
-    '"Sinopse": "Os Eternos são uma raça de seres imortais que viveram durante a antiguidade da Terra, moldando sua história e suas civilizações enquanto batalhavam os malignos Deviantes.",' +
-    '"Imagens": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/11/eternos-capa-2.jpg",' +
-    '"Personsagem": [' +
-    '{' +
-    '"Nome": "Ikaris",' +
-    '"Descricao": "Ikaris é um personagem das histórias em quadrinhos do Universo Marvel, publicado pela Marvel Comics. Ele apareceu pela primeira vez em The Eternals # 1 e foi criado por Jack Kirby. Ele é um membro dos Eternos, uma raça de super-humanos do Universo",' +
-    '"LinkImagem": "https://rollingstone.uol.com.br/media/_versions/richard_madden_como_ikaris_em_eternos_foto_reproducao__twitter_widelg.jpg"' +
-    '},' +
-    '{' +
-    '"Nome": "Cavaleiro Negro",' +
-    '"Descricao": "O Cavaleiro Negro é um personagem de história em quadrinhos da Marvel Comics. Sua primeira aparição foi em Avengers #47",' +
-    '"LinkImagem": "https://upload.wikimedia.org/wikipedia/pt/thumb/a/a1/Cavaleiro_Negro_por_Robert_Atkins.jpg/260px-Cavaleiro_Negro_por_Robert_Atkins.jpg"' +
-    '}' +
-    ']' +
-    '},' +
-    '{' +
-    '"NomeFilme": "Eternosssssss",' +
-    '"Sinopse": "Os Eternos são uma raça de seres imortais que viveram durante a antiguidade da Terra, moldando sua história e suas civilizações enquanto batalhavam os malignos Deviantes.",' +
-    '"Imagens": "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/11/eternos-capa-2.jpg",' +
-    '"Personsagem": [' +
-    '{' +
-    '"Nome": "Ikaris",' +
-    '"Descricao": "Ikaris é um personagem das histórias em quadrinhos do Universo Marvel, publicado pela Marvel Comics. Ele apareceu pela primeira vez em The Eternals # 1 e foi criado por Jack Kirby. Ele é um membro dos Eternos, uma raça de super-humanos do Universo",' +
-    '"LinkImagem": "https://rollingstone.uol.com.br/media/_versions/richard_madden_como_ikaris_em_eternos_foto_reproducao__twitter_widelg.jpg"' +
-    '},' +
-    '{' +
-    '"Nome": "Cavaleiro Negro",' +
-    '"Descricao": "O Cavaleiro Negro é um personagem de história em quadrinhos da Marvel Comics. Sua primeira aparição foi em Avengers #47",' +
-    '"LinkImagem": "https://upload.wikimedia.org/wikipedia/pt/thumb/a/a1/Cavaleiro_Negro_por_Robert_Atkins.jpg/260px-Cavaleiro_Negro_por_Robert_Atkins.jpg"' +
-    '}' +
-    ']' +
-    '}]';
+var filmes = [
+    {
+        NomeFilme: "Eternos",
+        Sinopse: "Os Eternos são uma raça de seres imortais que viveram durante a antiguidade da Terra, moldando sua história e suas civilizações enquanto batalhavam os malignos Deviantes.",
+        Imagens: "https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/11/eternos-capa-2.jpg",
+        Personagens: [
+            Nome = "Ikaris",
+            Descricao = "Ikaris é um personagem das histórias em quadrinhos do Universo Marvel, publicado pela Marvel Comics. Ele apareceu pela primeira vez em The Eternals # 1 e foi criado por Jack Kirby. Ele é um membro dos Eternos, uma raça de super-humanos do Universo",
+            LinkImagem = "https://rollingstone.uol.com.br/media/_versions/richard_madden_como_ikaris_em_eternos_foto_reproducao__twitter_widelg.jpg"
+        ],
+    },
+    {
+        NomeFilme: "Eternos",
+        Sinopse: "Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.",
+        Imagens: "https://www.blogsoestado.com/marciohenrique/files/2019/09/VINGADORES-ULTIMATO-2019.jpg",
+        Personagens: [
+            Nome = "Thanos",
+            Descricao = "Thanos é um personagem fictício, um supervilão das histórias em quadrinhos publicadas pela Marvel Comics, inspirado em Thanatos",
+            LinkImagem = "https://uploads.jovemnerd.com.br/wp-content/uploads/2018/04/thanos-hot-toy-1210x540.jpg"
+        ],
+    }
+]
 
 const obj = JSON.parse(filmes);
 
@@ -58,11 +45,11 @@ obj.forEach(e => {
     sinopse.innerHTML += '- ' + e.Sinopse
 
     fetch(e.Imagens)
-    .then( data => {        
-        return data.blob();
-    }).then( res => {
-        const imgURL = URL.createObjectURL(res)
-        const img = `<img src="${imgURL}"/>`;
-        document.querySelector('#filme').innerHTML = img;
-    })
+        .then(data => {
+            return data.blob();
+        }).then(res => {
+            const imgURL = URL.createObjectURL(res)
+            const img = `<img src="${imgURL}"/>`;
+            document.querySelector('#filme').innerHTML = img;
+        })
 });
